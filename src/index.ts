94c2025c1513024275fc4c938fc012c87e20096b
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-09-02 03:13:46 +0800

    feat: txt resolution

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-const waow = "no";
+import * as dns from "dns";
 
-console.log(waow);
+enum Lexicons {
+    APP_BSKY = "app.bsky.actor",
+    // CHAT_BSKY = "https://github.com/bluesky-social/atproto/tree/main/lexicons/chat/bsky",
+    COM_ATPROTO = "com.atproto.admin",
+    TOOLS_OZONE = "tools.ozone.communication",
+    // SH_TANGLED = "https://tangled.sh/@tangled.sh/core/tree/master/lexicons",
+    // PLACE_STREAM = "https://github.com/streamplace/streamplace/tree/next/lexicons",
+    // SOCIAL_GRAIN = "https://github.com/grainsocial/grain/tree/main/lexicons",
+    // COM_WHTWND = "https://github.com/whtwnd/whitewind-blog/tree/main/lexicons",
+}
+
+const resolveTxt = (domain: string) => {
+    const records = dns.resolveTxt(domain, (err, records) => {
+        records.forEach((record) => console.log(record));
+    });
+};
+
+const main = () => {
+    const lexiconDomains = Object.values(Lexicons);
+    lexiconDomains.forEach((domain) => {
+        const mirrored = domain.split(".").reverse().join(".");
+        resolveTxt(`_lexicon.${mirrored}`);
+    });
+};
+
+main();
