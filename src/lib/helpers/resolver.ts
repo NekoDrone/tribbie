01f5b58c1e0a172e88be347359afc1b100c053fa
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-09-05 01:01:55 +0800

    refactor: don't be an idiot
    what the fuck was i smoking when i wrote this. what do you mean async
    function but i'm using promise-chaining???

--- src/lib/helpers/resolver.ts
+++ src/lib/helpers/resolver.ts
@@ -2,26 +2,19 @@ import * as dns from "dns/promises";
 import type { PlcDirectoryResponse } from "@/lib/types";
 
 export const resolveTxt = async (domain: string): Promise<string> => {
-    let res = "";
-    return dns
-        .resolveTxt(domain)
-        .then((records) => {
-            records.forEach((record) => {
-                record.forEach((segment) => {
-                    if (segment.startsWith("did=")) {
-                        const kvp = segment.split("=");
-                        if (kvp.length >= 2) {
-                            res = kvp[1] as string;
-                        }
-                    }
-                });
-            });
-        })
-        .then(() => {
-            if (res != "") return res;
-
-            throw new Error(`No DID record found at ${domain}`);
+    const records = await dns.resolveTxt(domain);
+    records.forEach((record) => {
+        record.forEach((segment) => {
+            if (segment.startsWith("did=")) {
+                const kvp = segment.split("=");
+                if (kvp.length >= 2) {
+                    return kvp[1];
+                }
+            }
         });
+    });
+
+    throw new Error(`No DID record found at ${domain}`);
 };
 
 export const resolveDidToServiceUrl = async (did: string) => {
